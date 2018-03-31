abb17af28e4dc7c6deb45c4894766e6ee5062abe
girikalra <girikalra@gmail.com>
2018-03-31 15:18:35 -0700

    add api to tmdb, enable search bar to display list

--- ittdf/client/src/components/searchbar.jsx
+++ ittdf/client/src/components/searchbar.jsx
@@ -7,10 +7,14 @@ export default class SearchBar extends React.Component {
     super(props);
 
     this.state = {
-      term: ''
+      term: '',
+      results: [],
+      id: ''
     }
+    
     this.handleInput = this.handleInput.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
+    this.getRelevantData = this.getRelevantData.bind(this);
   }
 
   handleInput(q) {
@@ -20,18 +24,36 @@ export default class SearchBar extends React.Component {
   handleSearch() {
     axios.get('/searchMovie', { params: { title: this.state.term } })
       .then((response) => {
-        console.log(response);
+        this.setState({results: this.getRelevantData(response.data.results)})
       })
       .catch((err) => {
         console.log(err);
       })
   }
 
+  getRelevantData(movieList) {
+    var movies = []
+    movieList.forEach((movie) => {
+      movies.push({
+        title: movie.title,
+        year: movie.release_date.slice(0,4),
+        id: movie.id
+      });
+    })
+    return movies;
+  }
+
   render() {
+
+    const list = this.state.results.map((movie, index) =>
+      <li key={index}>{movie.title} ({movie.year})</li> 
+    );
+
     return (
       <div>
         <form> <input type="text" onChange={this.handleInput} /> </form>
         <button onClick={this.handleSearch}>search</button>
+        <ul>{list}</ul>
       </div>
     );
   }
--- ittdf/server/index.js
+++ ittdf/server/index.js
@@ -1,18 +1,31 @@
 require('../tmdb_api_key');
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const request = require('request');
 
 let app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.json())
 
-
+var options = { method: 'GET',
+  url: 'https://api.themoviedb.org/3/search/movie',
+  qs: 
+   { include_adult: 'false',
+     page: '1',
+     query: '',
+     language: 'en-US',
+     api_key: TMDB_API_KEY },
+  body: '{}' 
+};
 
 app.get('/searchMovie', ((req, res) => {
-  console.log(req.query.title);
-  res.send('giri')
+  options.qs.query = req.query.title;
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    else res.send(body);
+  });
 }));
 
 let port = 3000;
