a15be8ad2fec48ee57c614126c6af016d8097355
girikalra <girikalra@gmail.com>
2018-03-31 19:36:17 -0700

    Add ability to add and remove search bars

--- ittdf/client/src/components/searchcontainer.jsx
+++ ittdf/client/src/components/searchcontainer.jsx
@@ -3,16 +3,45 @@ import ReactDOM from 'react-dom';
 import SearchBar from './searchbar.jsx';
 
 export default class SearchContainer extends React.Component {
+  
   constructor(props) {
     super(props);
+    this.state = { num: 2 };
+    this.addSearchBars = this.addSearchBars.bind(this);
+    this.removeSearchBars = this.removeSearchBars.bind(this);
+  }
+
+  addSearchBars() {
+    if (this.state.num < 5) {
+      var num = this.state.num + 1;
+      this.setState({num: num});
+    }
+  }
+
+  removeSearchBars() {
+    if (this.state.num > 2) {
+      var num = this.state.num - 1;
+      this.setState({num: num});
+    }
   }
 
   render() {
+
+    const removeButton = [];
+    if (this.state.num > 2) {
+      removeButton.push(<button key={1} onClick={this.removeSearchBars}>remove movie searchbar</button>)
+    }
+
+    const bars = [];
+    for (var i = 0 ; i < this.state.num; i++) {
+      bars.push(<SearchBar key={i} getID={this.props.getID}/>)
+    }
+
     return (
       <div>
-        <div>Search Box</div>
-        <SearchBar getID={this.props.getID}/>
-        <SearchBar getID={this.props.getID}/>
+        <button onClick={this.addSearchBars}>add movie searchbar</button>
+        {removeButton}
+        {bars}
       </div>
     );
   }
