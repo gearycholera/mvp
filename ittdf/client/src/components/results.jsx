5b4d84e847c1a58e59ea021120c55fa5e70d10c8
girikalra <girikalra@gmail.com>
2018-03-31 18:47:22 -0700

    can now console.log the common actors and their roles

--- ittdf/client/src/components/results.jsx
+++ ittdf/client/src/components/results.jsx
@@ -6,19 +6,43 @@ export default class Results extends React.Component {
   constructor(props) {
     super(props);
     this.compareMovies = this.compareMovies.bind(this);
+    this.getRelevantCastData = this.getRelevantCastData.bind(this);
   }
 
   compareMovies() {
     var ids = this.props.ids;
+    var castsInfo = [];
     if (ids.length === 2) {
-      axios.get('/compareMovies', { params: { ids: ids } })
+      axios.get('/compareMovies', { params: { id1: ids[0], id2: ids[1] } })
       .then((response) => {
-        console.log(response);
+        response.data.forEach((res) => {
+          var movie = JSON.parse(res);
+          castsInfo.push(this.getRelevantCastData(movie.cast));
+        });
+        console.log(this.compareCasts(castsInfo))
       })
       .catch((error) => {
         console.log(error);
       })
     }
+  }
+
+  getRelevantCastData(castData) {
+    var castMembers = {};
+    castData.forEach((cast) => {
+      castMembers[cast.name] = cast.character;
+    })
+    return castMembers;
+  }
+
+  compareCasts(casts) {
+    var common = [];
+    for (var key in casts[0]) {
+      if (casts[1].hasOwnProperty(key)) {
+        common.push([key, casts[0][key], casts[1][key]])
+      }
+    }
+    return common;
   }
 
   render() {
--- ittdf/server/index.js
+++ ittdf/server/index.js
@@ -10,28 +10,51 @@ app.use(bodyParser.json())
 
 var movieSearch = { method: 'GET',
   url: 'https://api.themoviedb.org/3/search/movie',
-  qs: 
-   { include_adult: 'false',
-     page: '1',
-     query: '',
-     language: 'en-US',
-     api_key: TMDB_API_KEY },
+  qs: { 
+    include_adult: 'false',
+    page: '1',
+    query: '',
+    language: 'en-US',
+    api_key: TMDB_API_KEY
+  },
   body: '{}' 
 };
 
 app.get('/searchMovie', ((req, res) => {
   movieSearch.qs.query = req.query.title;
-
   request(movieSearch, function (error, response, body) {
     if (error) throw new Error(error);
     else res.send(body);
   });
-
 }));
 
+'https://api.themoviedb.org/3/movie/~/credits'
+
+var castSearch = { method: 'GET',
+  url: '',
+  qs: { 
+    api_key: TMDB_API_KEY 
+  },
+  body: '{}' 
+};
+
 app.get('/compareMovies', ((req, res) => {
-  console.log(req.query.ids);
-  res.send('compareThem');
+  var output = [];
+  castSearch.url = `https://api.themoviedb.org/3/movie/${req.query.id1}/credits`
+  request(castSearch, function (error, response, body) {
+    if (error) throw new Error(error);
+    else {
+      output.push(body);
+      castSearch.url = `https://api.themoviedb.org/3/movie/${req.query.id2}/credits`
+      request(castSearch, function (error, response, body) {
+        if (error) throw new Error(error);
+        else {
+          output.push(body);
+          res.send(output);
+        }
+      });
+    }
+  });
 }));
 
 let port = 3000;
